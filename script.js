90acea6e7ce76b9572acad801f5f46e3cbf3e051
Викторов Павел Валерьянович <decidedtosolvethisproblem@gmail.com>
2024-10-07 18:02:46 +0300

    Added a script for uploading cards

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const btn = document.querySelector(".load-items");
 const itemContainer = document.querySelector(".main-content");
-
-btn.addEventListener("click",()=>{
-    for(i=0;i<10;i++){
-        const item = document.createElement("div");
-        item.innerHTML = "<h2>Заголовок</h2>";
-        item.classList.add("flag");
-        itemContainer.appendChild(item);
+let count = 3;
+    btn.addEventListener("click",()=>{
+    count -= 1;
+    if(count>0){
+    for(i=0;i<8;i++){
+      btn.insertAdjacentHTML('beforebegin',`<figure class="item">
+                    <img src="img/card-photo.png"                  alt="item-card">
+                    <figcaption>
+                        <h2>Sun-Glass</h2>
+                        <p>Current bid</p>
+                        <img src="img/card-romb.svg" alt="romb">
+                        <p>1.75</p>
+                        <button>PLACE BID</button>
+                        <div>07h 09m 012s</div>
+                    </figcaption>
+                </figure>
+                `)        
     }
-
-    
-
+   }
+   else{
+    btn.diabled = true;
+    btn.textContent = 'No more cards'
+}
 })
